702697fe6b004cb185aea564d4b40e027821da58
ABBOTT-LAP\Abbott <abbottf@verizon.net>
2020-12-29 12:35:23 -0500

    Still have problem with objects in buffer

--- FIFO_CB_Test/Enums.h
+++ FIFO_CB_Test/Enums.h
@@ -12,51 +12,7 @@
 class EnumsClass
 {
  public:
-	  
-	 enum SapLineState {
-		 DONE,
-		 CLOSE_ALL,	// On throughout close, until all are closed
-		 OPENNING,	// Open line to test
-		 CLOSING,	// Close leaking line
-		 NEXT_LINE,	// Go to next line at next iterration
-	 };
 
-	 enum JsonType {
-		 LOG_DATA,
-		 TEMPERATUE_DATA,
-		 VACUUM_DATA,
-	 };
-	 enum State {
-		 Cold,
-		 NoFlow,
-		 Running,
-		 Starting,
-		 Stopped,
-		 Stopping,
-		 VacuumClean,
-	 };
-	 enum Action {
-		FlowCheck,
-		HeatInRange,
-		HeatOff,
-		HeatOn, 
-		Initializing,
-		LineCheck,
-		NoAction,
-		ShutDown,
-		StartUp,
-		ToCold,
-		ToWarm,
-		VaccumCheck,
-	 };
-
-	 enum Temperature {
-		 InCold,
-		 InHot,
-		 InInRange,
-		 OutCold,
-		 OutInRange,
-	 };
 
 	 enum Interupt{
 		CheckInTemp,
@@ -68,10 +24,6 @@ class EnumsClass
 		LifoPush,
 	 };
 
-	 static String EnumStr(Action action);
-	 static String EnumStr(JsonType jsonType);
-	 static String EnumStr(State state);
-	 static String EnumStr(Temperature temperature);
 	 static String EnumStr(Interupt interrupt);
 };
 
